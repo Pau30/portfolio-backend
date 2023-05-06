33c557557ea06dc2931e3c85a4e3f459d2f0d64f
Paula <pacaviglia@gmail.com>
2023-05-05 21:41:05 -0300

    cont red y contacto

---
+++ src/main/java/com/miproyect/portfolio/controller/ContContacto.java
@@ -0,0 +1,41 @@
+package com.miproyect.portfolio.controller;
+
+import com.miproyect.portfolio.entity.Contacto;
+import com.miproyect.portfolio.service.SerContacto;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("contacto")
+@CrossOrigin(origins = "http://localhost:4200")
+public class ContContacto {
+      @Autowired
+    public SerContacto SContacto;
+              
+    @GetMapping("/list")
+    @ResponseBody
+    public List<Contacto> listContacto() {
+        return SContacto.listContacto();
+    }
+
+    @GetMapping("/search/{id}")
+    @ResponseBody
+    public Contacto verContacto(@PathVariable int id) {
+        return SContacto.buscarContacto(id);
+    }
+
+ 
+    @PutMapping("/edit")
+    public void updateContacto(@RequestBody Contacto contacto) {
+        SContacto.editarContacto(contacto);
+
+    }
+}
---
+++ src/main/java/com/miproyect/portfolio/controller/ContRed.java
@@ -0,0 +1,55 @@
+package com.miproyect.portfolio.controller;
+
+import com.miproyect.portfolio.entity.Red;
+import com.miproyect.portfolio.service.SerRed;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("redes")
+@CrossOrigin(origins = "http://localhost:4200")
+public class ContRed {
+      @Autowired
+    public SerRed SRed;
+              
+    @GetMapping("/list")
+    @ResponseBody
+    public List<Red> listRed() {
+        return SRed.listRed();
+    }
+
+    @GetMapping("/search/{id}")
+    @ResponseBody
+    public Red verRed(@PathVariable int id) {
+        return SRed.buscarRed(id);
+    }
+
+    
+    @PostMapping("/add")
+    public void agregarRed(@RequestBody Red red) {
+        SRed.crearRed(red);
+    }
+
+    
+    @DeleteMapping("/delete/{id}")
+    public void eliminarRed(@PathVariable int id) {
+        SRed.borrarRed(id);
+
+    }
+  
+    @PutMapping("/edit")
+    public void updateRed(@RequestBody Red red) {
+        SRed.editarRed(red);
+
+    }
+}
